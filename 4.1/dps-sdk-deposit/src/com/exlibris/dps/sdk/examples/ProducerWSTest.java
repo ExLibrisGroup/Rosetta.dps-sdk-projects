75ca9b9d4556b966a733b6768909f2718f4f9b25
Ex Libris Code Share <exlcodeshare@users.noreply.github.com>
2015-03-08 10:32:13 +0200

    Update ProducerWSTest.java

--- 4.1/dps-sdk-deposit/src/com/exlibris/dps/sdk/examples/ProducerWSTest.java
+++ 4.1/dps-sdk-deposit/src/com/exlibris/dps/sdk/examples/ProducerWSTest.java
@@ -30,8 +30,8 @@ public class ProducerWSTest {
 		/* get pds handle */
 		String pdsHandle = null;
 
-		String userName = "admin1";
-		String password = "a12345678A";
+		String userName = "user123";
+		String password = "pass123";
 		String institution = "INS00";
 
 		try {
