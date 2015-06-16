85157405abf3d4f1e0a0355e724c5888cf7eaf79
Ex Libris Code Share <api-team@exlibrisgroup.com>
2015-06-16 16:36:48 +0300

    Adding javadoc pages

--- current/dps-sdk-deposit/src/com/exlibris/dps/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://dps.exlibris.com/")
-package com.exlibris.dps;
--- current/dps-sdk-plugins/resources/bin/unzip_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/csh
-
-set OUTPUT_DIR="$1"
-set FILE_NAME="$2"
-
-unzip -o -P aaaa -d "$OUTPUT_DIR" "$FILE_NAME"
